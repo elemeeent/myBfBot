9f70dbc8832a4f8a73f2875158304aec088f496f
Viacheslav Sosudin <v.sosudin@semrush.com>
2019-06-19 11:46:29 +0300

    update concatPlayerName method...

--- src/main/java/helpers/MessageHelpers.java
+++ src/main/java/helpers/MessageHelpers.java
@@ -152,11 +152,13 @@ public class MessageHelpers {
     }
 
     private static String concatPlayerName(String name) {
+        log.info("Concat player: {}", name);
         name = name.replaceAll("\\)", "");
         name = name.replaceAll("\\(.*", "");
         if (name.contains(" ")) {
             name = name.replaceAll("\\s+", "");
         }
+        log.info("Final name: {}", name);
         return name;
     }
 
